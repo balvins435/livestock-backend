7fe14f25e04fa7a2d12632fb791b5800572519e2
Mwangimark <markcosmars2000@gmail.com>
2025-12-05 15:48:50 +0300

    added venv in gitignore and adjusted the predict url

--- disease_prediction/urls.py
+++ disease_prediction/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import DiseasePredictionAPI
 
 urlpatterns = [
-    path("predict/", DiseasePredictionAPI.as_view(), name="predict-disease"),
+    path("predict", DiseasePredictionAPI.as_view(), name="predict-disease"),
 ]
